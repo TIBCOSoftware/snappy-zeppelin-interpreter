e0b0d4309f53a555c3e88b00a1c1bfbb17487012
Sachin <sjanani@snappydata.io>
2016-10-10 09:05:18 +0530

    [SNAP-1121] Changes related to connection pooling and setting some default properties

---
+++ src/main/java/org/apache/zeppelin/interpreter/Constants.java
@@ -0,0 +1,24 @@
+package org.apache.zeppelin.interpreter;
+/*
+ * Copyright (c) 2016 SnappyData, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You
+ * may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License. See accompanying
+ * LICENSE file.
+ */
+public class Constants {
+
+  public static final String FS_S3A_ACCESS_KEY = "fs.s3a.access.key";
+  public static final String FS_S3A_SECRET_KEY = "fs.s3a.secret.key";
+  public static final String FS_S3A_IMPL = "fs.s3a.impl";
+  public static final String SPARK_SQL_SHUFFLE_PARTITIONS = "spark.sql.shuffle.partitions";
+}
