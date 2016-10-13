888f64b5c2ecfb4343b4d0aaf8c837b8f29fa181
SachinJanani <sjanani@snappydata.io>
2016-10-13 17:30:40 +0530

    [SNAP-1126] Fix for SNAP-1126 to set default max s3 connections to 1000 and allow user to specify that property in interpreter setting (#7)

--- src/main/java/org/apache/zeppelin/interpreter/Constants.java
+++ src/main/java/org/apache/zeppelin/interpreter/Constants.java
@@ -21,4 +21,5 @@ public class Constants {
   public static final String FS_S3A_SECRET_KEY = "fs.s3a.secret.key";
   public static final String FS_S3A_IMPL = "fs.s3a.impl";
   public static final String SPARK_SQL_SHUFFLE_PARTITIONS = "spark.sql.shuffle.partitions";
+  public static final String FS_S3A_CONNECTION_MAXIMUM = "fs.s3a.connection.maximum";
 }
